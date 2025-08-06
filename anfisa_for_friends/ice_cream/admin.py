e98bc4cbbc3c8e610c3eadba323e38b46b618538
Dima Fomenko <fomenkodmitriy94@yandex.ru>
2025-08-06 14:51:50 +0300

    commit

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, IceCream, Topping, Wrapper
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'     
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    filter_horizontal = ('toppings',)
+    list_display_links = ('title',)
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (IceCreamInline,)
+    last_display = ('title',)
+
+admin.site.empty_value_display = 'Не задано'
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
